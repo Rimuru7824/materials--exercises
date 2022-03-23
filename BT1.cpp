612d4b86c3ed5e7a8587dc407d5bf6f71b70819d
ᵛᶰシHιℓℓ²ᵏ⁶ <94588387+Rimuru7824@users.noreply.github.com>
2022-03-24 05:45:26 +0700

    Update BT1.cpp

--- BT1.cpp
+++ BT1.cpp
@@ -1,10 +1,3 @@
-/*
-Bài tập 1: Viết chương trình tính giai thừa của số n với yêu cầu : 
-- nếu người dùng nhập n < 0 thì yêu cầu nhập lại 
-- sử dụng chương trình con để tính giai thừa của số n
- + n! = 1 nếu n = 0
- + n! = 1*2*3*4*5*....*n (tích của n thừa số)
-*/
 #include <bits/stdc++.h>
 using namespace std;
 
