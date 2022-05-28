dcf26f46e7258d9b4904afb5b4d0789130b557b1
ᵛᶰMιикυαи <94588387+Rimuru7824@users.noreply.github.com>
2022-05-28 15:48:03 +0700

    Update QHD1.cpp

--- QHD1.cpp
+++ QHD1.cpp
@@ -6,9 +6,22 @@ vector<int> a;
 
 int l[100];
 int t[100];
-void QHD(int n)
+void QHD(vector<int> arr, int n)
 {
-    
+    l[n] = 1;
+    for(int i=n;i>=0;i--)
+    {
+        int jmax = i;
+        for(int j=i+1;j<=n;j++)
+        {
+            if(arr[i] <= arr[j] && l[j] > l[jmax])
+            {
+                jmax = j;
+                l[i] = l[jmax] + 1;
+                t[i] = jmax;
+            }
+        }
+    }
 }
 int GTLN(vector<int> arr, int n)
 {
@@ -27,19 +40,28 @@ int GTNN(vector<int> arr, int n)
     Min--;
     return Min;
 }
-void insert_array()
+void insert_array(int Min, int Max)
 {
     a.insert(a.begin(),Min);
     a.push_back(Max);
 }
 int main()
 {
-    int n = 8;
-    a = {1,5,9,3,20,7,10,3};
+    int n = 10;
+    a = {1,2,3,4,9,10,5,6,7,8};
     int Min, Max ;
     Max =  GTLN(a,n);
     Min = GTNN(a,n);
-    insert_array();
-    
+    insert_array(Min,Max);
+    int N = a.size() - 2;
+    QHD(a,N);
+    cout << "maximum length of subarray : " << l[0] -1 << endl;
+    cout << "longest monotonically increasing array : " ;
+    int i=0;
+    while(t[i]< N)
+    {
+        cout << a[t[i]] << " ";
+        i = t[i];
+    }
     return 0;
 }
